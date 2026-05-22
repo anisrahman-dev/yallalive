bc268a7698167452a06eb41647aa70f3def925ed
Anisur Rahman <anisrahman.contact@gmail.com>
2026-05-22 16:14:51 +0600

    chore(matches): admin update (6 matches)

--- matches.js
+++ matches.js
@@ -1,22 +1,5 @@
 const MATCHES_DATA = [
   {
-    id: 1,
-    league: "Copa America",
-    time: "3:00 AM",
-    status: "",
-    day: "today",
-    isImportant: true,
-    servers: ["https://t.me","#","#"],
-    homeTeam: {
-      name: "Brazil",
-      logo: "logos/brazil.png"
-    },
-    awayTeam: {
-      name: "Argentina",
-      logo: "logos/argentina.png"
-    }
-  },
-  {
     id: 2,
     league: "Copa America",
     time: "3:00 AM",
