f39cc40f215fdc6dcf73c8cdafe4609fc33d7a6f
Anisur Rahman <anisrahman.contact@gmail.com>
2026-05-23 12:43:57 +0600

    chore(matches): admin update (1 matches)

--- matches.js
+++ matches.js
@@ -1,104 +1,19 @@
 const MATCHES_DATA = [
   {
-    id: 2,
-    league: "Copa America",
-    time: "3:00 AM",
+    id: 1,
+    league: "Italy, Serie A",
+    time: "10:00 AM",
     status: "",
     day: "today",
     isImportant: true,
-    servers: ["https://t.me"],
+    servers: ["#"],
     homeTeam: {
-      name: "Uruguay",
-      logo: "logos/uruguay.png"
+      name: "Bologna",
+      logo: "logos/bologna.png"
     },
     awayTeam: {
-      name: "Colombia",
-      logo: "logos/colombia.png"
-    }
-  },
-  {
-    id: 3,
-    league: "UEFA Champions League",
-    time: "3:00 AM",
-    status: "",
-    day: "today",
-    isImportant: true,
-    servers: ["https://t.me"],
-    homeTeam: {
-      name: "Crystal Palace",
-      logo: "logos/crystal-palace.png"
-    },
-    awayTeam: {
-      name: "Eintracht Frankfurt",
-      logo: "logos/eintracht-frankfurt.png"
-    }
-  },
-  {
-    id: 4,
-    league: "Spain, La Liga",
-    time: "",
-    status: "",
-    day: "today",
-    isImportant: true,
-    servers: ["https://t.me"],
-    homeTeam: {
-      name: "Girona",
-      logo: "logos/girona.png"
-    },
-    awayTeam: {
-      name: "Real Oviedo",
-      logo: "logos/real-oviedo.png"
-    }
-  },
-  {
-    id: 5,
-    league: "Club Friendly",
-    time: "",
-    status: "",
-    day: "today",
-    isImportant: true,
-    servers: ["https://t.me"],
-    homeTeam: {
-      name: "Manchester United",
-      logo: "logos/man-united.png"
-    },
-    awayTeam: {
-      name: "Everton",
-      logo: "logos/everton.png"
-    }
-  },
-  {
-    id: 6,
-    league: "Spain, La Liga",
-    time: "3:00 AM",
-    status: "",
-    day: "today",
-    isImportant: true,
-    servers: ["https://t.me"],
-    homeTeam: {
-      name: "Getafe",
-      logo: "logos/getafe.png"
-    },
-    awayTeam: {
-      name: "Rayo Vallecano",
-      logo: "logos/rayo-vallecano.png"
-    }
-  },
-  {
-    id: 7,
-    league: "England, English Premier League",
-    time: "3:00 AM",
-    status: "",
-    day: "today",
-    isImportant: true,
-    servers: ["https://t.me"],
-    homeTeam: {
-      name: "Fulham",
-      logo: "logos/fulham.png"
-    },
-    awayTeam: {
-      name: "Crystal Palace",
-      logo: "logos/crystal-palace.png"
+      name: "Inter Milan",
+      logo: "logos/inter-milan.png"
     }
   }
 ];
