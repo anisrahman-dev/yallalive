39e268530f5ea1ce70b63d59c4fe9f989e77c8e6
Anisur Rahman <anisrahman.contact@gmail.com>
2026-06-16 00:07:24 +0600

    chore(matches): admin update (3 matches)

--- public/matches.js
+++ public/matches.js
@@ -2,18 +2,52 @@ const MATCHES_DATA = [
   {
     id: 1,
     league: "FIFA World Cup 2026",
-    time: "10:00 PM",
+    time: "1:00 AM",
     status: "",
     day: "today",
     isImportant: true,
     servers: ["https://www.yallalivefootball.com/tv-1.html"],
     homeTeam: {
-      name: "Spain",
-      logo: "logos/spain.png"
+      name: "Belgium",
+      logo: "logos/belgium.png"
     },
     awayTeam: {
-      name: "Cape Verde",
-      logo: "logos/cameroon.svg"
+      name: "Egypt",
+      logo: "logos/egypt.png"
+    }
+  },
+  {
+    id: 2,
+    league: "FIFA World Cup 2026",
+    time: "4:00 AM",
+    status: "",
+    day: "today",
+    isImportant: true,
+    servers: ["https://www.yallalivefootball.com/tv-1.html"],
+    homeTeam: {
+      name: "Saudi Arabia",
+      logo: "logos/saudi-arabia.png"
+    },
+    awayTeam: {
+      name: "Uruguay",
+      logo: "logos/uruguay.png"
+    }
+  },
+  {
+    id: 3,
+    league: "FIFA World Cup 2026",
+    time: "7:00 AM",
+    status: "",
+    day: "today",
+    isImportant: true,
+    servers: ["https://www.yallalivefootball.com/tv-1.html"],
+    homeTeam: {
+      name: "Iran",
+      logo: "logos/iran.png"
+    },
+    awayTeam: {
+      name: "New Zealand",
+      logo: "logos/new-zealand.png"
     }
   }
 ];
