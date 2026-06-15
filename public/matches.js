4fab3cb0aee395183cf1f31da4de390b70f05792
Anisur Rahman <anisrahman.contact@gmail.com>
2026-06-15 21:54:00 +0600

    chore(matches): admin update (1 matches)

--- public/matches.js
+++ public/matches.js
@@ -1,2 +1,19 @@
 const MATCHES_DATA = [
+  {
+    id: 1,
+    league: "FIFA World Cup 2026",
+    time: "10:00 PM",
+    status: "",
+    day: "today",
+    isImportant: true,
+    servers: ["https://www.yallalivefootball.com/tv-1.html"],
+    homeTeam: {
+      name: "Spain",
+      logo: "logos/spain.png"
+    },
+    awayTeam: {
+      name: "Cape Verde",
+      logo: "logos/cameroon.svg"
+    }
+  }
 ];
