a45b8647d43e9f0ff20351b52bce1aba319ec938
Anisur Rahman <anisrahman.contact@gmail.com>
2026-06-15 21:32:42 +0600

    chore(matches): admin update (1 matches)

--- public/matches.js
+++ public/matches.js
@@ -2,62 +2,11 @@ const MATCHES_DATA = [
   {
     id: 1,
     league: "FIFA World Cup 2026",
-    time: "11:00 PM",
+    time: "10:00 PM",
     status: "",
     day: "today",
     isImportant: true,
-    servers: ["https://yallalive.sx/"],
-    homeTeam: {
-      name: "Germany",
-      logo: "logos/germany.png"
-    },
-    awayTeam: {
-      name: "Curacao",
-      logo: "logos/ecuador.png"
-    }
-  },
-  {
-    id: 2,
-    league: "FIFA World Cup 2026",
-    time: "2:00 AM",
-    status: "",
-    day: "today",
-    isImportant: true,
-    servers: ["https://yallalive.sx/"],
-    homeTeam: {
-      name: "Netherlands",
-      logo: "logos/netherlands.png"
-    },
-    awayTeam: {
-      name: "Japan",
-      logo: "logos/japan.png"
-    }
-  },
-  {
-    id: 3,
-    league: "FIFA World Cup 2026",
-    time: "5:00 AM",
-    status: "",
-    day: "today",
-    isImportant: true,
-    servers: ["https://yallalive.sx/"],
-    homeTeam: {
-      name: "Ivory Coast",
-      logo: "logos/ivory-coast.png"
-    },
-    awayTeam: {
-      name: "Ecuador",
-      logo: "logos/ecuador.png"
-    }
-  },
-  {
-    id: 4,
-    league: "FIFA World Cup 2026",
-    time: "10:30 PM",
-    status: "",
-    day: "today",
-    isImportant: true,
-    servers: ["https://yallalive.sx/"],
+    servers: ["#"],
     homeTeam: {
       name: "Spain",
       logo: "logos/spain.png"
