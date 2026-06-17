3250ee8284ba657e5b9e173835a10f7b53b18939
Anisur Rahman <anisrahman.contact@gmail.com>
2026-06-17 23:02:35 +0600

    chore(tv): channel servers updated (3 server links)

--- public/tv-channels.js
+++ public/tv-channels.js
@@ -2,7 +2,7 @@ window.TV_CHANNELS = [
   {
     id: 1,
     title: "TV 1",
-    servers: ["https://tv-trt1-esdai.medya.trt.com.tr/master_1440p.m3u8", "https://tfws3gr3uomttgr31hp3tfa3dqmmeqfsg17uff3hsnhuqcp1ghmucpf5gk.100ycdn.com/hqlive.yarncdn.live/live/hqtv_blv_phanma/playlist.m3u8?wsSession=3389cfe769edf9bcd6f19c64-178163730861652&wsIPSercert=f7884ab7e197b7a0915eef641dd1876d&wsBindIP=2&wsserid=1168201446950356247", "https://live12.szyac.com/live/85042987.m3u8"]
+    servers: ["https://tfwt1gradgmttgy41hqbifaaugcjppbasa4mscwz81am1p3tse5tqptwzc3e.100ycdn.com/hqlive.yarncdn.live/live/hqtv_blv_phanma/playlist.m3u8?wsSession=939129847ae2540ded65b86c-178171572544639&wsIPSercert=f7884ab7e197b7a0915eef641dd1876d&wsBindIP=2&wsserid=1168240832916737938", "https://tfwt1gradgmttgy41hqbifaaugcjppbasa4mscwz81am1p3tse5tqptwzc3e.100ycdn.com/hqlive.yarncdn.live/live/hqtv_blv_phanma/playlist.m3u8?wsSession=939129847ae2540ded65b86c-178171572544639&wsIPSercert=f7884ab7e197b7a0915eef641dd1876d&wsBindIP=2&wsserid=1168240832916737938", "https://tfwt1gradgmttgy41hqbifaaugcjppbasa4mscwz81am1p3tse5tqptwzc3e.100ycdn.com/hqlive.yarncdn.live/live/hqtv_blv_phanma/playlist.m3u8?wsSession=939129847ae2540ded65b86c-178171572544639&wsIPSercert=f7884ab7e197b7a0915eef641dd1876d&wsBindIP=2&wsserid=1168240832916737938"]
   },
   {
     id: 2,
