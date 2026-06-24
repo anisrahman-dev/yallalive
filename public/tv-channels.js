86a99e0212a21fabf3cf886b5bdbe5104a08cbb8
Anisur Rahman <anisrahman.contact@gmail.com>
2026-06-25 00:20:46 +0600

    chore(tv): channel servers updated (3 server links)

--- public/tv-channels.js
+++ public/tv-channels.js
@@ -2,7 +2,7 @@ window.TV_CHANNELS = [
   {
     id: 1,
     title: "TV 1",
-    servers: ["https://live13a.jynrrs.com/live/20233432.m3u8", "https://live13a.jynrrs.com/live/16697898.m3u8", "https://tfwy3gr3uomttgr31hp3tfa3dqmmeqfsg17uff3hsnhuqcp1ghmucpf5gk.100ycdn.com/hqlive.yarncdn.live/live/hqtv_blv_phanliem/playlist.m3u8?wsSession=3389cfe769edf9bcd6f19c64-178214695611843&wsIPSercert=f7884ab7e197b7a0915eef641dd1876d&wsBindIP=2&wsserid=1168201446950356247"]
+    servers: ["https://live13a.jynrrs.com/live/20233432.m3u8", "https://live13a.jynrrs.com/live/16697898.m3u8", "https://live13a.jynrrs.com/live/79635460.m3u8"]
   },
   {
     id: 2,
