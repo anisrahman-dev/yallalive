23670cdcf268d48b13934b67d4c6e8311bb140ed
Anisur Rahman <anisrahman.contact@gmail.com>
2026-07-04 10:46:56 +0600

    chore(tv): channel servers updated (0 server links)

--- public/tv-channels.js
+++ public/tv-channels.js
@@ -2,7 +2,7 @@ window.TV_CHANNELS = [
   {
     id: 1,
     title: "TV 1",
-    servers: ["https://live01-cn-ali.bvjicd.com/live/43639555.m3u8", "https://tfx83gr3uomttgr31hp3tfa3dqmmcpf5gkmuqcf38cpbqcpzs4.100ycdn.com/live.narv4.com/mlive/M999-HD.m3u8?wsSession=7a5b26cffc70c2a2102ec403-178301738934972&wsIPSercert=f7884ab7e197b7a0915eef641dd1876d&wsBindIP=2&wsserid=1168201446952453455", "https://pull.niues.live/live/stream-9912108_lhd.m3u8?auth_key=1782953603-0-0-b9dbb91b3f6c4ee0f4ee8f7fe1052ed4"]
+    servers: ["", "", ""]
   },
   {
     id: 2,
